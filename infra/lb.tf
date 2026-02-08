48cee5fb181faddefad2b7fc4e2588514012095d
Aleksuo <aleksi.suoranta@gmail.com>
2026-02-08 20:35:47 +0200

    Change the lb to be private

--- infra/lb.tf
+++ infra/lb.tf
@@ -1,3 +1,7 @@
+locals {
+  lb_ip = cidrhost(var.private_network_subnet_range, 10)
+}
+
 resource "hcloud_load_balancer" "controlplane_load_balancer" {
   name               = "talos-lb"
   load_balancer_type = var.load_balancer_type
@@ -7,6 +11,8 @@ resource "hcloud_load_balancer" "controlplane_load_balancer" {
 resource "hcloud_load_balancer_network" "srvnetwork" {
   load_balancer_id = hcloud_load_balancer.controlplane_load_balancer.id
   network_id       = hcloud_network.private_network.id
+  enable_public_interface = false
+  ip = local.lb_ip
 }
 
 resource "hcloud_load_balancer_target" "control_plane_target" {
@@ -15,6 +21,8 @@ resource "hcloud_load_balancer_target" "control_plane_target" {
   server_id        = hcloud_server.controlplane_server.id
   use_private_ip   = true
   depends_on = [
+    hcloud_load_balancer_network.srvnetwork,
+    hcloud_network_subnet.private_network_subnet,
     hcloud_server.controlplane_server
   ]
 }

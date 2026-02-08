aafb3e53c710a82c8d9499c3176d1f10502e32b1
Aleksuo <aleksi.suoranta@gmail.com>
2026-02-08 20:39:49 +0200

    Configure a public nat gateway for the cluster with an auto approved tailscale setup

---
+++ infra/firewall.tf
@@ -0,0 +1,17 @@
+resource "hcloud_firewall" "nat_gateway_firewall" {
+  name = "${var.cluster_name}-nat-gateway"
+
+   rule {
+    direction  = "in"
+    protocol   = "tcp"
+    port       = "any"
+    source_ips = [var.private_network_subnet_range]
+  }
+
+  rule {
+    direction  = "in"
+    protocol   = "udp"
+    port       = "any"
+    source_ips = [var.private_network_subnet_range]
+  }
+}
---
+++ infra/nat_gateway.tf
@@ -0,0 +1,31 @@
+locals {
+  nat_ip = cidrhost(var.private_network_subnet_range, 20)
+}
+
+resource "hcloud_server" "nat_gateway" {
+  name = "${var.cluster_name}-nat-gateway"
+  image = "ubuntu-24.04"
+  server_type = "cx23"
+  location = var.location
+
+  firewall_ids = [hcloud_firewall.nat_gateway_firewall.id]
+
+  network {
+    network_id = hcloud_network.private_network.id
+    ip = local.nat_ip
+  }
+
+  user_data = templatefile("${path.module}/cloud-init/nat-gateway.yaml.tftpl", {
+    private_network_subnet_range = var.private_network_subnet_range
+    tailscale_auth_key           = tailscale_tailnet_key.nat_gateway_key.key
+  })
+
+  depends_on = [ tailscale_acl.nat_gateway_acl ]
+}
+
+resource "hcloud_network_route" "default_via_nat" {
+  network_id = hcloud_network.private_network.id
+  destination = "0.0.0.0/0"
+  gateway = local.nat_ip
+  depends_on = [ hcloud_server.nat_gateway ]
+}

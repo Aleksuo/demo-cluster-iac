ef8312b8287e884bdfbe4b5fedee7587ad15eb5a
Aleksi <aleksi.suoranta@gmail.com>
2026-02-22 20:56:12 +0200

    Forward traffic from the tofu provisioned private lb -> cilium gateway api gateway

--- infra/lb.tf
+++ infra/lb.tf
@@ -41,4 +41,16 @@ resource "hcloud_load_balancer_service" "controlplane_load_balancer_service_talo
   destination_port = 50000
 }
 
+resource "hcloud_load_balancer_service" "controlplane_load_balancer_service_gateway_http" {
+  load_balancer_id = hcloud_load_balancer.controlplane_load_balancer.id
+  protocol         = "tcp"
+  listen_port      = 80
+  destination_port = 30080
+}
 
+resource "hcloud_load_balancer_service" "controlplane_load_balancer_service_gateway_https" {
+  load_balancer_id = hcloud_load_balancer.controlplane_load_balancer.id
+  protocol         = "tcp"
+  listen_port      = 443
+  destination_port = 30443
+}

e4f2ea6e4ce244fe2bf635beb835db2b1b71e6fa
Aleksi <aleksi.suoranta@gmail.com>
2026-02-28 11:57:37 +0200

    Prevent shell mangling the secret literals in bootstrap external secrets script

--- scripts/bootstrap/02_bootstrap-external-secrets.sh
+++ scripts/bootstrap/02_bootstrap-external-secrets.sh
@@ -2,4 +2,8 @@
 set -euo pipefail
 
 kubectl create namespace external-secrets --dry-run=client -o yaml | kubectl apply -f -
-kubectl create secret generic infisical-universal-auth --from-literal=clientId=$EXTERNAL_SECRETS_CLIENT_ID --from-literal=clientSecret=$EXTERNAL_SECRETS_CLIENT_SECRET
+kubectl create secret -n external-secrets generic infisical-universal-auth \
+  --from-literal=clientId="${EXTERNAL_SECRETS_CLIENT_ID}" \
+  --from-literal=clientSecret="${EXTERNAL_SECRETS_CLIENT_SECRET}" \
+  --dry-run=client \
+  -o yaml | kubectl apply -f -
